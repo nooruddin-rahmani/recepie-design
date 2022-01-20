04c42c3ac594d190742985ce50621a7a3c58f430
Nooruddin <nooruddin.rahmani100@gmail.com>
2022-01-20 08:59:02 +0430

    changes made to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ["Montserrat", "sans-serif"],
+        "play-Fair": ["Playfair Display"],
+      },
+      spacing: {
+        592: "37rem",
+        969: "60.5625rem",
+        216: "13.5rem",
+        530: "33.125rem",
+        367: "22.9375rem",
+      },
+      margin: {
+        18: "1.125rem",
+      },
+      textColor: {
+        orange: "#F2994A",
+      },
+    },
   },
   plugins: [],
 };
